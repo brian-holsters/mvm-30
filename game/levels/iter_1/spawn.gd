596bd9f7ec4b2d357da9d647f86a3f150f9f1d53
Brian <brian.holsters@gmail.com>
2025-11-25 12:30:03 +0100

    fixed crash in spawn room

--- game/levels/iter_1/spawn.gd
+++ game/levels/iter_1/spawn.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 var player: MvmPlayer
-@onready var text_player: Node = $TextPlayer
-@onready var text_player_2: Node = $TextPlayer2
+
+@onready var intro_text_player: Node = %IntroTextPlayer
+@onready var open_door_text_player: Node = %OpenDoorTextPlayer
 @onready var gate: TileMapLayer = %Gate
 @onready var text_played_flag: FlagNode = $TextPlayedFlag
 
@@ -11,7 +12,7 @@ var intro_text_finished = false
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player") as MvmPlayer
 	player.state_machine.state_changed.connect(_on_player_state_machine_changed)
-	text_player.play_timeline()
+	intro_text_player.play_timeline()
 	await Dialogic.signal_event
 	intro_text_finished = true
 
@@ -21,6 +22,6 @@ func _on_player_state_machine_changed(from: StringName, to: StringName):
 			if text_played_flag.get_flag_value() or not intro_text_finished:
 				return
 			text_played_flag.set_flag()
-			text_player_2.play_timeline()
+			open_door_text_player.play_timeline()
 			await Dialogic.signal_event
 			gate.queue_free()
